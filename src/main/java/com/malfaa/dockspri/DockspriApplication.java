815e87628461d0de9282863ba29abe49c885ee10
Malfaa <gmalfacorrea@gmail.com>
2024-02-06 23:58:39 +0000

    first commit

---
+++ src/main/java/com/malfaa/dockspri/DockspriApplication.java
@@ -0,0 +1,13 @@
+package com.malfaa.dockspri;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DockspriApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DockspriApplication.class, args);
+	}
+
+}
